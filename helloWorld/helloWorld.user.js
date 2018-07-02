87ef490075aec341042bb619be3c666ee5a52e93
takashi-kimura <40040546+takashi-kimura@users.noreply.github.com>
2018-07-03 08:05:35 +0900

    Update helloWorld.user.js

--- helloWorld/helloWorld.user.js
+++ helloWorld/helloWorld.user.js
@@ -1,40 +1,55 @@
-if(typeof(dojo) != "undefined") { 
-  require(["dojo/domReady!"], function(){ 
-    try { 
-      // utility function to wait for a specific element to load... 
-      var waitFor = function(callback, eXpath, eXpathRt, maxIV, waitTime){ 
-        if(!eXpathRt) var eXpathRt = dojo.body(); 
-        if(!maxIV) var maxIV = 10000; // intervals before expiring 
-        if(!waitTime) var waitTime = 1; // 1000=1 second 
-        if(!eXpath) return; 
-        var waitInter = 0; // current interval 
-        var intId = setInterval( function(){ 
-          if(++waitInter < maxIV && !dojo.query(eXpath,eXpathRt).length) 
-           return; 
-  
-          clearInterval(intId); 
-          if( waitInter >= maxIV) { 
-            console.log("**** WAITFOR ["+eXpath+"] WATCH EXPIRED!!! interval "+waitInter+" (max:"+ maxIV +")"); 
-          } else { 
-            console.log("**** WAITFOR ["+eXpath+"] WATCH TRIPPED AT interval "+waitInter+" (max:"+maxInter+")"); 
-            callback(); 
-          } 
-        }, waitTime); // end setInterval() 
-      }; // end waitFor() 
-  
-      // here we use waitFor to wait for the 
-      // .lotusStreamTopLoading div.loaderMain.lotusHidden element 
-      // before we proceed to customize the page... 
-      waitFor( function(){ 
-        // wait until the "loading..." node has been hidden 
-        // indicating that we have loaded content. 
-  
-        dojo.query("span.shareSome-title")[0].textContent="Hello World Sample!!!! "; 
-  
-      }, ".lotusStreamTopLoading div.loaderMain.lotusHidden"); 
-  
-    } catch(e) { 
-      alert("Exception occurred in helloWorld: " + e); 
-    } 
-  }); 
- }
+// ==UserScript==
+// @copyright    Copyright IBM Corp. 2017
+//
+// @name         helloWorld
+// @version      0.1
+// @description  *** PROTOTYPE CODE *** demonstrates simple hello world script to customize the Home Page
+//
+// @namespace  http://ibm.com
+//
+// @author       Hello World (aka You!)
+//
+// @include      *://apps.collabservintegration.com/homepage/*
+//
+// @exclude
+//
+// @run-at       document-end
+//
+// ==/UserScript==
+
+if(typeof(dojo) != "undefined") {
+	require(["dojo/domReady!"], function(){
+        try {
+            // utility function to let us wait for a specific element of the page to load...
+            var waitFor = function(callback, elXpath, elXpathRoot, maxInter, waitTime) {
+                if(!elXpathRoot) var elXpathRoot = dojo.body();
+                if(!maxInter) var maxInter = 10000;  // number of intervals before expiring
+                if(!waitTime) var waitTime = 1;  // 1000=1 second
+                if(!elXpath) return;
+                var waitInter = 0;  // current interval
+                var intId = setInterval( function(){
+                    if( ++waitInter<maxInter && !dojo.query(elXpath,elXpathRoot).length) return;
+
+                    clearInterval(intId);
+                    if( waitInter >= maxInter) { 
+                        console.log("**** WAITFOR ["+elXpath+"] WATCH EXPIRED!!! interval "+waitInter+" (max:"+maxInter+")");
+                    } else {
+                        console.log("**** WAITFOR ["+elXpath+"] WATCH TRIPPED AT interval "+waitInter+" (max:"+maxInter+")");
+                        callback();
+                    }
+                }, waitTime);
+            };
+
+            // here we use waitFor to wait on the .lotusStreamTopLoading div.loaderMain.lotusHidden element
+            // before we proceed to customize the page...
+            waitFor( function(){
+			// wait until the "loading..." node has been hidden
+			// indicating that we have loaded content.
+   			dojo.query("span.shareSome-title")[0].textContent="Hello World! ";
+       	          },
+		  ".lotusStreamTopLoading div.loaderMain.lotusHidden");
+      } catch(e) {
+          alert("Exception occurred in helloWorld: " + e);
+      }
+   });
+}
